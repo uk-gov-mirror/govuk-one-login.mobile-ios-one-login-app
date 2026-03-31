1e100fa9b947dff58c6e7ceb11dd3a7176644e4e
Markos Zoulias Charatzas <Markos.Charatzas@dsit.gov.uk>
2026-03-31 12:21:00 +0100

    DCMAW-18499 improve reliability of UI tests (#747)
    * feat: DCMAW-18499 improve reliability of UI Tests
    * stop execution after failure so that the test process fails as soon as possible to avoid running in excess
    * deterministically wait for the app to be running on the foreground rather than at an arbitrary time limit
    * ensure that each test uses its own instance of the `XCUIApplication` to operate to provide stronger guarantees on the state it operates
    * add assertions on wait for existence calls so as to fail early
    * remove redundant wait for existence calls
    * add wait for existence calls where required (i.e. waiting for a *specific* element to appear on screen, say a tabbar for home, rather than a broader one like "any" view)
    * add wait for the lock screen to disappear as a GIVEN for every test
    test_loginHappyPath: remove the assertion on the "Loading" label since the app does not make strong guarantees on its existence for the UI testing thus can randomly fail.
    test_loginCancelPath: will now assert that the signInButton *exists* as a way to verify that the sign in screen is shown when cancel is tapped. This is a more reliable way than a scroll view being "hittable" as it is more lenient than requiring the view to exist *and* can be clicked, tapped, or pressed at its current location

--- Tests/UITests/ScreenObjects/ScreenObject.swift
+++ Tests/UITests/ScreenObjects/ScreenObject.swift
@@ -8,9 +8,4 @@ extension ScreenObject {
     var isVisible: Bool {
         view.isHittable
     }
-    
-    func waitForAppearance(timeout: TimeInterval = .timeout) -> Self {
-        _ = view.waitForExistence(timeout: timeout)
-        return self
-    }
 }
