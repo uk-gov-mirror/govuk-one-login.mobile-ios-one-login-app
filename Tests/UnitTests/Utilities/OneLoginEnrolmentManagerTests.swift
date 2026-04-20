0232fd1c5adaf5fd01dd6c203f0c317cd78201c8
Markos Zoulias Charatzas <markos.charatzas@digital.cabinet-office.gov.uk>
2026-04-20 17:23:53 +0100

    fix: DCMAW-19786 fixed a case where the deeplink is not handled
    	This is a case where the WalletCoordinator, that is used by the
    	LocalAuthServiceWallet is also a child of the TabManagerCoordinator.
    
    	Calling `finish()` on the WalletCoordinator, will remove it from
    	the arrary of childCoordinators in the TabManagerCoordinator.
    
    	Thus, any future calls to handleUniversalLink will fail silently
    	due to the WalletCoordinator being nil.
    
    	This code change makes two assumptions that might be invalid in the future
    	thus causing this defect to resurface or be the reason of a new defect.
    
    	Assumption 1:
    	The WalletCoordinator is indeed a child of the TabManagerCoordinator when the finish is called.
    
    	Assumption 2:
    	The call to finish is at a direct conflict with the invariants of the TabManagerCoordinator
    	that require the WalletCoordinator to be present for the lifecycle of the TabManagerCoordinator
    
    	Should a new requirement arise in the future that expects the call to WalletCoordinator.finish()
    	to have been made, any future code carries a risk of being blindsided by this fix at worse or have
    	to grapple with the same decision, whether to call finish or not and under what conditions, at best.

--- Tests/UnitTests/Utilities/OneLoginEnrolmentManagerTests.swift
+++ Tests/UnitTests/Utilities/OneLoginEnrolmentManagerTests.swift
@@ -1,8 +1,51 @@
 import Coordination
 import LocalAuthenticationWrapper
+import Networking
 @testable import OneLogin
 import XCTest
 
+class MockChildCoordinatorExpectation: ChildCoordinator {
+    
+    weak var parentCoordinator: (any Coordination.ParentCoordinator)?
+    
+    typealias StartAsFunction = () -> Void
+    typealias FinishAsFunction = () -> Void
+    
+    var startAsFunction: StartAsFunction
+    var finishAsFunction: FinishAsFunction
+
+    init(startAsFunction: @escaping StartAsFunction = {}, finishAsFunction: @escaping FinishAsFunction = {}) {
+        self.startAsFunction = startAsFunction
+        self.finishAsFunction = finishAsFunction
+    }
+    
+    func start() {
+        self.startAsFunction()
+    }
+    
+    func finish() {
+        self.finishAsFunction()
+    }
+}
+
+extension OneLoginEnrolmentManager {
+    static func make(mockLocalAuthContext: LocalAuthManaging = MockLocalAuthManager(),
+                     mockSessionManager: SessionManager = MockSessionManager(),
+                     mockAnalyticsService: OneLoginAnalyticsService = MockAnalyticsService(),
+                     coordinator: ChildCoordinator? = nil) -> OneLoginEnrolmentManager {
+        let coordinator = coordinator ?? EnrolmentCoordinator(
+            root: UINavigationController(),
+            analyticsService: mockAnalyticsService,
+            sessionManager: mockSessionManager
+        )
+        return OneLoginEnrolmentManager(
+            localAuthContext: mockLocalAuthContext,
+            sessionManager: mockSessionManager,
+            analyticsService: mockAnalyticsService,
+            coordinator: coordinator
+        )
+    }
+}
 final class OneLoginEnrolmentManagerTests: XCTestCase {
     private var mockLocalAuthContext: MockLocalAuthManager!
     private var mockSessionManager: MockSessionManager!
@@ -101,5 +144,70 @@ extension OneLoginEnrolmentManagerTests {
         waitForTruth(self.mockLocalAuthContext.didCallEnrolFaceIDIfAvailable, timeout: 5)
         // THEN an error is recorded in Crashlytics
         XCTAssertEqual(mockAnalyticsService.crashesLogged, [MockError.generic as NSError])
+    }
+
+    @MainActor
+    func test_saveSession_isWalletEnrolmentTrue_callsFinishOnCoordinator() {
+        let expectation = expectation(description: #function)
+        expectation.isInverted = true
+        let mockChildCoordinatorExpectation = MockChildCoordinatorExpectation(finishAsFunction: {
+                expectation.fulfill()
+            })
+        
+        let sut: OneLoginEnrolmentManager = .make(coordinator: mockChildCoordinatorExpectation)
+        // WHEN saveSession is called
+        sut.saveSession(isWalletEnrolment: true)
+        wait(for: [expectation], timeout: 1)
+    }
+    
+    @MainActor
+    func test_saveSession_isWalletEnrolmentFalse_finishOnCoordinatorNotCalled() {
+        let expectation = expectation(description: #function)
+        let mockChildCoordinatorExpectation = MockChildCoordinatorExpectation(finishAsFunction: {
+                expectation.fulfill()
+            })
+        
+        let sut: OneLoginEnrolmentManager = .make(coordinator: mockChildCoordinatorExpectation)
+        // WHEN saveSession is called
+        sut.saveSession(isWalletEnrolment: false)
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    @MainActor
+    func test_saveSession_default_finishOnCoordinatorNotCalled() {
+        let expectation = expectation(description: #function)
+        let mockChildCoordinatorExpectation = MockChildCoordinatorExpectation(finishAsFunction: {
+                expectation.fulfill()
+            })
+        
+        let sut: OneLoginEnrolmentManager = .make(coordinator: mockChildCoordinatorExpectation)
+        // WHEN saveSession is called
+        sut.saveSession()
+        wait(for: [expectation], timeout: 5)
+    }
+    
+    @MainActor
+    func test_saveSession_isWalletEnrolmentTrue__walletCoordinator_notRemoved_asChild() {
+        let expectation = expectation(description: #function)
+        let tabManagerCoordinator = TabManagerCoordinator(root: UITabBarController(),
+                                        analyticsService: mockAnalyticsService,
+                                        networkingService: NetworkClient(),
+                                        sessionManager: mockSessionManager)
+        
+        let walletCoordinator =  WalletCoordinator(analyticsService: mockAnalyticsService,
+                                               networkingService: NetworkClient(),
+                                               sessionManager: mockSessionManager)
+        
+        tabManagerCoordinator.childCoordinators.append(walletCoordinator)
+        walletCoordinator.parentCoordinator = tabManagerCoordinator
+
+
+        let sut: OneLoginEnrolmentManager = .make(coordinator: walletCoordinator)
+        // WHEN saveSession is called
+        sut.saveSession(isWalletEnrolment: true) {
+            expectation.fulfill()
+        }
+        wait(for: [expectation], timeout: 5)
+        XCTAssert(tabManagerCoordinator.childCoordinators.count == 1)
     }
 }
